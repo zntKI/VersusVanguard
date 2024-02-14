8e0a6ec66e68d1734864670dddd6742bf10c975e
Jerpharney <jerpharney@gmail.com>
2024-02-14 02:36:10 +0100

    ui class populated

---
+++ GXPEngine2023c/GXPEngine/Ui/Ui.cs
@@ -0,0 +1,15 @@
+using System;
+using GXPEngine.Core;
+
+class Ui : GameObject
+{
+    public Ui()
+    {
+        Console.WriteLine("Ui initialized");
+    }
+
+    void Update()
+    {
+        // Empty
+    }
+}
