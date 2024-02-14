dc00207f615487fa68ebc1371bd85f9f0d4c13fe
Jerpharney <jerpharney@gmail.com>
2024-02-14 13:02:24 +0100

    adding placeholders for level configs

--- GXPEngine2023c/GXPEngine/Ui/Ui.cs
+++ GXPEngine2023c/GXPEngine/Ui/Ui.cs
@@ -5,14 +5,37 @@ class Ui : GameObject
 {
 
     // menuTile array
+    MenuTile[] menuTiles = new MenuTile[3];
+    
+
     // dynamically load menuTiles
-    // test
 
     public Ui()
     {
+        // NOTE : This is a placeholder for the menuTiles untill the json configs are implemented
+        menuTiles[0] = new MenuTile();
+        menuTiles[1] = new MenuTile();
+        menuTiles[2] = new MenuTile();
+        menuTiles[3] = new MenuTile();
+
         // title image
+        menuTiles[0] = new MenuTile();
+        menuTiles[1] = new MenuTile();
+        menuTiles[2] = new MenuTile();
+        menuTiles[3] = new MenuTile();
+
         // background image
+        menuTiles[0] = new MenuTile();
+        menuTiles[1] = new MenuTile();
+        menuTiles[2] = new MenuTile();
+        menuTiles[3] = new MenuTile();
+
         // background sound
+        menuTiles[0] = new MenuTile();
+        menuTiles[1] = new MenuTile();
+        menuTiles[2] = new MenuTile();
+        menuTiles[3] = new MenuTile();
+
     }
 
     void Update()
