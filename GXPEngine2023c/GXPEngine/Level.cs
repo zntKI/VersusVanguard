26dfc241b743d06b914816e13c3ff77e4d63d91c
zntKI <kaloyanivanov@hotmail.com>
2024-02-14 11:42:38 +0100

    Implemented Level class

---
+++ GXPEngine2023c/GXPEngine/Level.cs
@@ -0,0 +1,26 @@
+﻿using GXPEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Level : GameObject
+{
+    private int bpm;
+	private List<Sound> melody;
+
+	private int counterBeats;
+
+	public Level(int bpm)
+	{
+		this.bpm = bpm;
+
+		counterBeats = 0;
+    }
+
+	private void Update()
+	{ 
+		//Spawn the tile with the random sound from the list based on bpm
+	}
+}
