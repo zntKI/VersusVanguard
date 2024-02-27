606e7cbf7cbf182690d4f129fa469fcbaeda39a4
Jerpharney <jerpharney@gmail.com>
2024-02-27 12:37:58 +0100

    json

--- GXPEngine/GXPEngine/AddOns/FileReader.cs
+++ GXPEngine/GXPEngine/AddOns/FileReader.cs
@@ -5,8 +5,26 @@ namespace GXPEngine
 {
     public class FileReader
     {
-        public FileReader()
+
+        String filename;
+        
+
+        public FileReader( string filename)
         {
+            this.filename = filename;
+        }
+
+        // public string[] ReadFile()
+        // {
+        //     string[] lines = System.IO.File.ReadAllLines(filename);
+        //     return lines;
+        // }
+
+        // read json data from file
+        public void ReadJson()
+        {
+            // string json = ;
+            // Console.WriteLine(json);
         }
     }
 }
