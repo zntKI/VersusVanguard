ce69fed62ee26a6f0d380915a658626ba94a1e0a
Jerpharney <jerpharney@gmail.com>
2024-02-28 02:07:10 +0100

    Revert "json"
    This reverts commit 606e7cbf7cbf182690d4f129fa469fcbaeda39a4.

--- GXPEngine/GXPEngine/AddOns/FileReader.cs
+++ GXPEngine/GXPEngine/AddOns/FileReader.cs
@@ -5,26 +5,8 @@ namespace GXPEngine
 {
     public class FileReader
     {
-
-        String filename;
-        
-
-        public FileReader( string filename)
+        public FileReader()
         {
-            this.filename = filename;
-        }
-
-        // public string[] ReadFile()
-        // {
-        //     string[] lines = System.IO.File.ReadAllLines(filename);
-        //     return lines;
-        // }
-
-        // read json data from file
-        public void ReadJson()
-        {
-            // string json = ;
-            // Console.WriteLine(json);
         }
     }
 }
