e28448a2856e96819c33bd0dab23de2a6da9c45d
zntKI <kaloyanivanov@hotmail.com>
2024-02-28 12:24:43 +0100

    Fixed stroke tile behavior

--- GXPEngine/Tiles/StrokeTile.cs
+++ GXPEngine/Tiles/StrokeTile.cs
@@ -17,6 +17,8 @@ public class StrokeTile : Tile
     private Tuple<bool, int> shouldStopMoving = new Tuple<bool, int>(false, 0);
     private int currentTileStoppedY;
 
+    private bool hasAlreadyStopped;
+
     public StrokeTile(string filename, bool isLeft, float speed, Vector2 leftDiscCoor, Vector2 rightDiscCoor, bool shouldMoveLeft, string soundPath, float strokeLength) : base(filename, speed, leftDiscCoor, rightDiscCoor, shouldMoveLeft, soundPath)
     {
         this.isLeft = isLeft;
@@ -26,6 +28,8 @@ public class StrokeTile : Tile
         AddChild(stroke);
 
         stroke.SetXY(shouldMoveLeft ? strokeXOffset : -strokeXOffset, strokeYOffset);
+
+        hasAlreadyStopped = false;
     }
 
     private void Update()
@@ -48,14 +52,17 @@ public class StrokeTile : Tile
 
         //Checks if the player has stopped 'spinning' the record
         if (distanceFromRecordCenter <= reactionDistance && shouldStopMoving.Item1 && !Input.GetKey(shouldStopMoving.Item2))
+        {
             shouldStopMoving = new Tuple<bool, int>(false, 0);
+            hasAlreadyStopped = true;
+        }
 
         bool conditionLeftLane = shouldMoveLeft && ((isLeft && Input.GetKey(Key.A)) || (!isLeft && Input.GetKey(Key.D)));
         bool conditionRightLane = !shouldMoveLeft && ((isLeft && Input.GetKey(Key.J)) || (!isLeft && Input.GetKey(Key.L)));
         if ((conditionLeftLane || conditionRightLane)
             && distanceFromRecordCenter <= reactionDistance)
         {
-            if (!shouldStopMoving.Item1)
+            if (!shouldStopMoving.Item1 && !hasAlreadyStopped)
             {
                 int keyCode = conditionLeftLane ? (isLeft ? Key.A : Key.D) : (isLeft ? Key.J : Key.L);//Change that once we have the real controller
                 shouldStopMoving = new Tuple<bool, int>(true, keyCode);
@@ -65,7 +72,8 @@ public class StrokeTile : Tile
                     DetachStroke();
             }
 
-            return Time.time % 8 == 0 ? reactionDistance - distanceFromRecordCenter : 0;
+            if (!hasAlreadyStopped)
+                return Time.time % 8 == 0 ? reactionDistance - distanceFromRecordCenter : 0;
         }
         return 0;
     }
