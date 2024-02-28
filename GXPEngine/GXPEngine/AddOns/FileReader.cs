c0a9e6cd51e9b207cde29e29ff3be4e277e79995
Jerpharney <jerpharney@gmail.com>
2024-02-28 02:07:03 +0100

    fixing p3

--- GXPEngine/GXPEngine/AddOns/FileReader.cs
+++ GXPEngine/GXPEngine/AddOns/FileReader.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Xml;
 using GXPEngine;
 
 namespace GXPEngine
@@ -9,59 +7,24 @@ namespace GXPEngine
     {
 
         String filename;
-        List<String> modes = new List<string> { "json", "xml", "txt" };
-        String mode;
-        XmlDocument xmlDoc = new XmlDocument();
         
 
-        public FileReader( string filename, string mode = "xml")
+        public FileReader( string filename)
         {
             this.filename = filename;
-
-            if (!modes.Contains(mode))
-            {
-                throw new Exception("Invalid mode");
-            } else 
-            {
-                this.mode = mode;
-            }  
-        }
-        
-        public void ReadFile()
-        {
-            if (mode == "json") ReadJson();
-            if (mode == "xml")  ReadXml();
-            if (mode == "txt")  ReadTxt();
         }
 
+        // public string[] ReadFile()
+        // {
+        //     string[] lines = System.IO.File.ReadAllLines(filename);
+        //     return lines;
+        // }
 
-        private string[] ReadTxt()
+        // read json data from file
+        public void ReadJson()
         {
-            string[] lines = System.IO.File.ReadAllLines(filename);
-            return lines;
-        }
-
-        private void ReadJson()
-        {
-        //     string json = System.IO.File.ReadAllText(filename);
-        //     dynamic data = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
-        //     foreach (var item in data)
-        //     {
-        //         string assetsLocation = item.assetsLocation;
-        //         string songLocation = item.songLocation;
-        //         int bpm = item.bpm;
-        //         Level level = new Level(bpm);
-        //     }
-        }
-
-        private void ReadXml()
-        {
-            xmlDoc.Load(filename);
-            XmlNodeList nodeList = xmlDoc.DocumentElement.SelectNodes("LevelConfig/Level1");
-            foreach (XmlNode node in nodeList)
-            {
-                Console.WriteLine( node );
-            }
+            // string json = ;
+            // Console.WriteLine(json);
         }
     }
 }
--- GXPEngine/MyGame.cs
+++ GXPEngine/MyGame.cs
@@ -7,8 +7,8 @@ using GXPEngine.Core;
 public class MyGame : Game {  // Resolution is 1366 x 768
 	public MyGame() : base(1366, 768, false, false)     // Create a window that's 800x600 and NOT fullscreen
 	{
-		FileReader fileReader = new FileReader(this.assets + "/levelConfig.xml");		
-		fileReader.ReadFile();
+		FileReader fileReader = new FileReader(this.assets + "/levelAssets/level1/levelConfig.json");		
+		fileReader.ReadJson();
 		Ui ui = new Ui();
 		AddChild(ui);
     }
