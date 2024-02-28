9ce392b040d477a40b582a81969cb6a7bb04197d
zntKI <kaloyanivanov@hotmail.com>
2024-02-28 09:35:35 +0100

    Added input retrieval from Arduino

--- GXPEngine/ControllerManager.cs
+++ GXPEngine/ControllerManager.cs
@@ -3,6 +3,49 @@ using GXPEngine;
 using GXPEngine.Core;
 using System.IO.Ports;
 
-class ControllerManager
+public class ControllerManager
 {
+    private SerialPort port;
+
+    private int currentRecordRightValue;
+    private int currentRecordLeftValue;
+    private int currentButtonValue;
+
+    public ControllerManager(string portName, int baudRate=9600)
+	{
+        port = new SerialPort();
+        port.PortName = portName;
+        port.BaudRate = baudRate;
+        port.RtsEnable = true;
+        port.DtrEnable = true;
+        port.Open();
+    }
+
+    public void ReadInput()
+    {
+        string line = port.ReadLine();
+
+        if (line == "")
+            return;
+
+        string[] inputParts = line.Split(',');
+
+        if (inputParts.Length != 3)
+            return;
+
+
+        /*currentRecordRightValue = */int.TryParse(inputParts[0].Substring(inputParts[0].IndexOf(':') + 1), out currentRecordRightValue);
+        /*currentRecordLeftValue = */int.TryParse(inputParts[1].Substring(inputParts[1].IndexOf(':') + 1), out currentRecordLeftValue);
+        /*currentButtonValue = */int.TryParse(inputParts[2].Substring(inputParts[2].IndexOf(':') + 1), out currentButtonValue);
+
+        Console.WriteLine($"{inputParts[0]} {currentRecordRightValue}");
+
+        //try
+        //{
+        //}
+        //catch (Exception)
+        //{
+        //    throw new Exception("The provided input from the Arduino is not in the right format");
+        //}
+    }
 }
--- GXPEngine/MyGame.cs
+++ GXPEngine/MyGame.cs
@@ -1,24 +1,54 @@
-
 using System;
 using GXPEngine;
 using System.Drawing;
 using GXPEngine.Core;
+using System.IO.Ports;
 
 public class MyGame : Game {  // Resolution is 1366 x 768
+
+    private ControllerManager controllerManager;
+
 	public MyGame() : base(1366, 768, false, false)     // Create a window that's 800x600 and NOT fullscreen
 	{
-		//Ui ui = new Ui();
-		//AddChild(ui);
+        //controllerManager = new ControllerManager("COM3");
 
-		AddChild(new Level(60));
+        //Ui ui = new Ui();
+        //AddChild(ui);
+
+        AddChild(new Level(60));
 	}
 
 	void Update() {
-		// Empty
-	}
+        //controllerManager.ReadInput();
+    }
 
 	static void Main()                          
 	{
-		new MyGame().Start();                  
+        //controllerManager = new ControllerManager();
+
+        SerialPort port = new SerialPort();
+        port.PortName = "COM3";
+        port.BaudRate = 9600;
+        port.RtsEnable = true;
+        port.DtrEnable = true;
+        port.Open();
+        while (true)
+        {
+            string line = port.ReadLine(); // read separated values
+                                           //string line = port.ReadExisting(); // when using characters
+            if (line != "")
+            {
+                Console.WriteLine("Read from port: " + line);
+
+            }
+
+            if (Console.KeyAvailable)
+            {
+                ConsoleKeyInfo key = Console.ReadKey();
+                port.Write(key.KeyChar.ToString());  // writing a string to Arduino
+            }
+        }
+
+        new MyGame().Start();                  
 	}
 }
